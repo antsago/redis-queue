54d514054e175de0714f502faa4762d347956792
Antonio <sago.ant@gmail.com>
2020-02-10 17:59:19 +0100

    feat: dockerized processor

--- processor/queueClient.js
+++ processor/queueClient.js
@@ -3,7 +3,7 @@ const RedisSMQ = require("rsmq");
 class QueueClient {
   constructor(queueName) {
     this.name = queueName;
-    this.rsmq = new RedisSMQ({ host: "127.0.0.1", port: 6379, ns: "rsmq" });
+    this.rsmq = new RedisSMQ({ host: "redis", port: 6379, ns: "rsmq" });
   }
 
   // I'll assume that the queue is only used for this and that the message 

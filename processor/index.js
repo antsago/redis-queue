08c4edc320af73fd5fca6f32dce9623ee267d1e6
Antonio <sago.ant@gmail.com>
2020-02-10 17:19:29 +0100

    feat: added first version of file save

--- processor/index.js
+++ processor/index.js
@@ -1,6 +1,19 @@
+const fs = require('fs');
 const Queue = require('./queueClient');
 const Nmbrs = require('./nmbrsClient');
 const Extactor = require('./infoExtractor');
+
+async function saveData(data) {
+  return await new Promise((resolve, reject) => 
+    fs.writeFile("./records/test.json", JSON.stringify(data), (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    }),
+  );
+}
 
 async function main() {
   const queue = new Queue('commandqueue');
@@ -15,7 +28,8 @@ async function main() {
       const client = new Nmbrs(payload.user, payload.pass)
       const extractor = new Extactor(client)
 
-      const info = await extractor.extractInfo();
+      const data = await extractor.extractInfo();
+      await saveData(data)
       await queue.delete(message.id);
 
       console.log(`Finished processing ${message.id}`);
